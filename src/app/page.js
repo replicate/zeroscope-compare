07d1f2ca7b77b38b0bb0a885497b536d28c02686
Paul Hayes <fofr@users.noreply.github.com>
2023-06-30 14:01:35 +0100

    Add upscaled video

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Link from 'next/link';
 import VideoForm from "@/components/VideoForm";
 import { ArrowRightIcon } from '@heroicons/react/24/solid';
 import { Space_Grotesk } from 'next/font/google'
+import VideoPlayer from '@/components/VideoPlayer';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
@@ -9,8 +11,8 @@ export default function Home() {
   return (
     <main className="container max-w-6xl mx-auto md:px-8 px-4 py-4 pb-10 bg-white border-black min-h-screen">
       <h1 className={ `${spaceGrotesk.className} text-6xl mt-6 mb-12 `}>
-        <div className="font-bold">Zeroscope v2</div>
-        <span className="text-4xl text-slate-500">A settings guide by Replicate</span>
+        <div className="font-bold text-orange-500 -mb-2">Zeroscope v2</div>
+        <span className="text-4xl font-bold">A settings guide by Replicate</span>
       </h1>
 
       <div className="max-w-prose text-xl">
@@ -27,20 +29,20 @@ export default function Home() {
       <div className="max-w-prose text-xl mb-8">
         <p>Try changing <code>num_frames</code>, <code>guidance_scale</code> and <code>num_inference_steps</code> to see what happens.</p>
       </div>
-      <div className="flex justify-between w-full">
-        <div className="mr-4 w-1/2">
+      <div className="md:flex md:justify-between w-full">
+        <div className="md:mr-4 md:w-1/2 w-full">
           <VideoForm formId="form1" />
         </div>
-        <div className="ml-4 w-1/2">
+        <div className="md:mr-4 md:w-1/2 w-full md:block hidden">
           <VideoForm formId="form2" />
         </div>
       </div>
 
       <div className="max-w-prose text-xl">
-        <h2 className={ `${spaceGrotesk.className} font-bold text-4xl mt-20 mb-8`}>Settings to start with</h2>
+        <h2 className={ `${spaceGrotesk.className} font-bold text-4xl mt-20 mb-8`}>Recommended settings</h2>
 
         <h3 className={ `${spaceGrotesk.className} font-bold text-2xl mt-8 mb-4`}>Start with the 576w model</h3>
-        <p>Begin by generating videos using the <code>576w</code> model at 576x320 resolution. When you find a video that you like, upscale it to 1024x576 using the <code>xl</code> model.</p>
+        <p>Begin by generating videos using the <code>576w</code> model at 576x320 resolution. When you find a video that you like, <Link href="#upscaling">upscale it to 1024x576</Link> using the <code>xl</code> model.</p>
 
         <p>You could create a 1024x576 video directly by using the <code>xl</code> model, but this video will render with duplicate objects and will tend to have low coherency. It’s better to use a two-step process (unless you’re intentionally trying to get weirder results).</p>
 
@@ -58,6 +60,27 @@ export default function Home() {
         <p>How many inference steps will be used to generate your video. More steps give better quality and coherency, but takes longer to generate. Fewer steps will give a lower quality video but will generate quickly – a good setting for fast prompt experimentation. Going above 100 steps will not improve your video.</p>
 
         <p>Try setting <code>num_inference_steps</code> to 50 to start with.</p>
+
+        <h2 className={ `${spaceGrotesk.className} font-bold text-4xl mt-20 mb-8`} id="upscaling">Upscaling</h2>
+
+        <p>When you have your 576x320 video, you can upscale it with the <code>xl</code> model.</p>
+
+        <p>You should:</p>
+
+        <ul className="list-disc ml-6 mb-4">
+          <li>reuse the same prompt and negative prompt</li>
+          <li>set <code>init_video</code> to the video you want to upscale</li>
+          <li>pick an <code>init_weight</code>, try 0.2</li>
+          <li>use a 1024x576 resolution</li>
+        </ul>
+
+        <h3 className={ `${spaceGrotesk.className} font-bold text-2xl mt-8 mb-4`}>Upscaled video</h3>
+
+        <p>
+          <a href="https://replicate.com/p/dujigjlbxyagw5fxctrde6tthq">View an example prediction</a>
+        </p>
+
+        <VideoPlayer video="/videos/clown_fish_upscale.mp4" autoPlay={false} />
       </div>
     </main>
   );

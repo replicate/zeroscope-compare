fed0aee548802c8764bc09025a771fd09792b379
Paul Hayes <fofr@users.noreply.github.com>
2023-06-30 15:13:25 +0100

    Play videos inline on mobile

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoPlayer = ({ video, autoPlay = true }) => {
   return (
     <div className="w-full rounded-md w-full mb-4">
-      <video src={video} controls loop preload="auto" autoPlay={autoPlay} className="w-full rounded-md" />
+      <video src={video} playsInline controls loop preload="auto" autoPlay={autoPlay} className="w-full rounded-md" />
     </div>
   );
 };
